d91dc62b459469f041b3eb0447fdbe4f7cbb481c
tckaufma <louis.kaufmann@stud.hslu.ch>
2014-10-22 15:25:29 +0200

    STAND 22.10.14

--- FRDM/Sources/Events.h
+++ FRDM/Sources/Events.h
@@ -39,6 +39,7 @@
 #include "SW2.h"
 #include "ExtIntLdd2.h"
 #include "LedBit3.h"
+#include "BitIoLdd3.h"
 #include "BitIoLdd1.h"
 #include "LedBit1.h"
 #include "BitIoLdd6.h"
@@ -49,9 +50,11 @@
 #include "ExtIntLdd4.h"
 #include "SW5.h"
 #include "BitIoLdd8.h"
-#include "SW6.h"
 #include "BitIoLdd9.h"
+#include "SW6.h"
+#include "BitIoLdd10.h"
 #include "SW7.h"
+#include "ExtIntLdd6.h"
 #include "ExtIntLdd5.h"
 #include "AS1.h"
 #include "ASerialLdd1.h"
@@ -175,6 +178,82 @@ void SW4_OnInterrupt(void);
 */
 /* ===================================================================*/
 void Cpu_OnNMIINT(void);
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnError (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called when a channel error (not the error
+**         returned by a given method) occurs. The errors can be read
+**         using <GetError> method.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AS1_OnError(void);
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnRxChar (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after a correct character is received.
+**         The event is available only when the <Interrupt
+**         service/event> property is enabled and either the <Receiver>
+**         property is enabled or the <SCI output mode> property (if
+**         supported) is set to Single-wire mode.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AS1_OnRxChar(void);
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnTxChar (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after a character is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AS1_OnTxChar(void);
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnFullRxBuf (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called when the input buffer is full;
+**         i.e. after reception of the last character 
+**         that was successfully placed into input buffer.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AS1_OnFullRxBuf(void);
+
+/*
+** ===================================================================
+**     Event       :  AS1_OnFreeTxBuf (module Events)
+**
+**     Component   :  AS1 [AsynchroSerial]
+**     Description :
+**         This event is called after the last character in output
+**         buffer is transmitted.
+**     Parameters  : None
+**     Returns     : Nothing
+** ===================================================================
+*/
+void AS1_OnFreeTxBuf(void);
 
 /* END Events */
 
